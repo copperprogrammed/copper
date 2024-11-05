c83e2652217e0e42e744dcb3adc71dcf4a4c0aa7
copperprogrammed <copperprogrammed@gmail.com>
2024-11-05 21:19:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function home() {
+    document.getElementById("Logo").style.display = "flex";
+    document.getElementById("voorbeeld1").style.display = "none";
+    document.getElementById("voorbeeld2").style.display = "none";
+}
+
+function shop() {
+    document.getElementById("Logo").style.display = "none";
+    document.getElementById("voorbeeld1").style.display = "block";
+    document.getElementById("voorbeeld2").style.display = "block";
+}
