ca0baf2211d362911ba9c5d97253279469593412
copperprogrammed <copperprogrammed@gmail.com>
2024-11-07 12:07:05 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function home() {
     document.getElementById("voorbeeld1").style.display = "none";
     document.getElementById("voorbeeld2").style.display = "none";
     document.getElementById("voorbeeld3").style.display = "none";
+    document.getElementById("voorbeeld4").style.display = "none";
+    document.getElementById("voorbeeld5").style.display = "none";
+    document.getElementById("voorbeeld6").style.display = "none";
+    document.getElementById("options").style.display = "none";
 }
 
 function shop() {
@@ -10,4 +14,74 @@ function shop() {
     document.getElementById("voorbeeld1").style.display = "block";
     document.getElementById("voorbeeld2").style.display = "block";
     document.getElementById("voorbeeld3").style.display = "block";
+    document.getElementById("voorbeeld4").style.display = "block";
+    document.getElementById("voorbeeld5").style.display = "block";
+    document.getElementById("voorbeeld6").style.display = "block";
+    document.getElementById("options").style.display = "block";
+}
+
+function show() {
+    document.getElementById("Logo").style.display = "none";
+    document.getElementById("voorbeeld1").style.display = "block";
+    document.getElementById("voorbeeld2").style.display = "block";
+    document.getElementById("voorbeeld3").style.display = "block";
+    document.getElementById("voorbeeld4").style.display = "block";
+    document.getElementById("voorbeeld5").style.display = "block";
+    document.getElementById("voorbeeld6").style.display = "block";
+    document.getElementById("options").style.display = "block";
+}
+
+function image() {
+    document.getElementById("Logo").style.display = "none";
+    document.getElementById("voorbeeld1").style.display = "block";
+    document.getElementById("voorbeeld2").style.display = "none";
+    document.getElementById("voorbeeld3").style.display = "none";
+    document.getElementById("voorbeeld4").style.display = "none";
+    document.getElementById("voorbeeld5").style.display = "none";
+    document.getElementById("voorbeeld6").style.display = "block";
+    document.getElementById("options").style.display = "block";
+}
+
+function info() {
+    document.getElementById("Logo").style.display = "none";
+    document.getElementById("voorbeeld1").style.display = "none";
+    document.getElementById("voorbeeld2").style.display = "block";
+    document.getElementById("voorbeeld3").style.display = "none";
+    document.getElementById("voorbeeld4").style.display = "none";
+    document.getElementById("voorbeeld5").style.display = "none";
+    document.getElementById("voorbeeld6").style.display = "none";
+    document.getElementById("options").style.display = "block";
+}
+
+function music() {
+    document.getElementById("Logo").style.display = "none";
+    document.getElementById("voorbeeld1").style.display = "none";
+    document.getElementById("voorbeeld2").style.display = "none";
+    document.getElementById("voorbeeld3").style.display = "block";
+    document.getElementById("voorbeeld4").style.display = "none";
+    document.getElementById("voorbeeld5").style.display = "none";
+    document.getElementById("voorbeeld6").style.display = "none";
+    document.getElementById("options").style.display = "block";
+}
+
+function video() {
+    document.getElementById("Logo").style.display = "none";
+    document.getElementById("voorbeeld1").style.display = "none";
+    document.getElementById("voorbeeld2").style.display = "none";
+    document.getElementById("voorbeeld3").style.display = "none";
+    document.getElementById("voorbeeld4").style.display = "block";
+    document.getElementById("voorbeeld5").style.display = "none";
+    document.getElementById("voorbeeld6").style.display = "none";
+    document.getElementById("options").style.display = "block";
+}
+
+function link() {
+    document.getElementById("Logo").style.display = "none";
+    document.getElementById("voorbeeld1").style.display = "none";
+    document.getElementById("voorbeeld2").style.display = "none";
+    document.getElementById("voorbeeld3").style.display = "none";
+    document.getElementById("voorbeeld4").style.display = "none";
+    document.getElementById("voorbeeld5").style.display = "block";
+    document.getElementById("voorbeeld6").style.display = "none";
+    document.getElementById("options").style.display = "block";
 }
